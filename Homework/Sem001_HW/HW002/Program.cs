724059e89e546320337e1ea86290cd4c3ff078bc
Maxim Shevtsov <maximshevtsov@Maxims-MBP.fritz.box>
2022-04-25 22:44:48 +0200

    HW_Sem01

---
+++ Homework/Sem001_HW/HW002/Program.cs
@@ -0,0 +1,21 @@
+﻿/*: Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел.*/
+Console.WriteLine("Input 3 numbers (with ENTER): ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine()); 
+
+if (num1 >= num2){
+    if(num1 >= num3){
+        Console.WriteLine("max = "+ num1);
+    }
+    else{
+        Console.WriteLine("max = "+ num3); 
+    }
+}
+else if(num2 >= num3){
+    Console.WriteLine("max = "+ num2); 
+}
+else{
+    Console.WriteLine("max = "+ num3); 
+}

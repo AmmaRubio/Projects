708d33ddd1c5cfe7410485fab594f8d9dc35bc97
Maxim Shevtsov <maximshevtsov@Maxims-MBP.fritz.box>
2022-04-25 22:10:47 +0200

    seminar 1 added

---
+++ Seminars/Sem001.1b/Program.cs
@@ -0,0 +1,7 @@
+﻿int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2 | number2 == number1 * number1)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
+ 
---
+++ Seminars/Sem001.7/Program.cs
@@ -0,0 +1,22 @@
+﻿/*4. Напишите программу, которая на вход принимает одно число
+(N), а на выходе показывает все целые числа в промежутке от -N
+до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> " -2, -1, 0, 1, 2"*/
+
+Console.WriteLine("Input a number");
+int N = Convert.ToInt32(Console.ReadLine()); 
+int Nnew = (-1)* N; 
+if(Nnew <= N){
+    while(Nnew <= N)
+    {
+        Console.Write(Nnew+ " "); 
+        Nnew++; 
+    }
+}
+else{
+    while(N <= Nnew){
+        Console.Write(N+" ");
+        N++; 
+    }
+}

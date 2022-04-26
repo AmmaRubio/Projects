a5b6a84130ea50514a4c8eefd67851a1ec0a3c5d
Maxim Shevtsov <maximshevtsov@Maxims-MBP.fritz.box>
2022-04-26 19:41:12 +0200

    _

--- Homework/Sem001_HW/HW002/Program.cs
+++ Homework/Sem001_HW/HW002/Program.cs
@@ -5,7 +5,7 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = Convert.ToInt32(Console.ReadLine());
 int num3 = Convert.ToInt32(Console.ReadLine()); 
 
-if (num1 >= num2){
+if (num1 >= num2){ 
     if(num1 >= num3){
         Console.WriteLine("max = "+ num1);
     }
@@ -19,3 +19,4 @@ else if(num2 >= num3){
 else{
     Console.WriteLine("max = "+ num3); 
 }
+//Наверно, лучше было бы делать через int max...
--- Lesson/Project004_Deviding/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿double numA = 12;
-double numB = 5; 
-Console.WriteLine(numA/numB); 

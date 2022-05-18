37b8941b50b3d9b6c760b7765d5271b4d5ec7f42
ge64kem <ge64kem@mytum.de>
2022-05-18 07:07:55 +0300

    hw003

---
+++ Homework/Sem003_HW/HW_001/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да*/
+
+Console.WriteLine("input any integer"); 
+int input = Convert.ToInt32(Console.ReadLine());
+
+Boolean ispolindrom(int input)
+{
+    string array = input.ToString();
+    Boolean result = true;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (!array[i].Equals(array[array.Length - i - 1]))
+        {
+            result = false;
+        }
+    }
+    return result; 
+}
+string booleanToYesorNo(Boolean x){
+    String result = "no"; 
+    if(x == true) result = "yes"; 
+    return result; 
+}
+
+String output = booleanToYesorNo(ispolindrom(input)); 
+Console.WriteLine($"{input} -> {output}"); 
+
---
+++ Homework/Sem003_HW/HW_002/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+double euqlidieanDistance3D(int a, int b, int c)
+{
+    double result = 0;
+    result = Math.Sqrt((a * a) + (b * b) +(c * c));
+    return result; 
+}
+
+Console.WriteLine("input X coordinate "); 
+int xCoor = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("input Y coordinate "); 
+int yCoor = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("input Z coordinate "); 
+int zCoor = Convert.ToInt32(Console.ReadLine()); 
+
+double distance = euqlidieanDistance3D(xCoor,yCoor,zCoor);
+
+Console.WriteLine($"distance between points is: {distance}");
+
+

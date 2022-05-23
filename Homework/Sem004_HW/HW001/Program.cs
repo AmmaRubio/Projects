34b45d6041524541f1885faadb9100a218534d82
ge64kem <ge64kem@mytum.de>
2022-05-23 18:15:30 +0200

    HW 4 is done

---
+++ Homework/Sem004_HW/HW001/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.*/
+Console.WriteLine("input number:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("another number:");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int powerAB(int a, int b)
+{
+    int result = a;
+    for (int i = 1; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+int output = powerAB(numA, numB);
+Console.WriteLine($"{numA}, {numB} -> {output}");
